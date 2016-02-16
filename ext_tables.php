c8452b4107af7f0a1821ac5318205c5eaf3ef991
Claus Due <claus@namelesscoder.net>
2016-02-16 18:15:58 +0100

    Add custom TCA-based image context fields to file metadata

--- ext_tables.php
+++ ext_tables.php
@@ -21,3 +21,14 @@ if (TYPO3_MODE === 'BE')
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Semantic Images');
+
+$GLOBALS['TCA']['sys_file_metadata']['columns']['image_context'] = array(
+	'label' => 'Image contextualisation',
+	'displayCond' => 'USER:Dkd\\SemanticImages\\UserFunction\\TcaFieldUserFunction->isFieldEnabled',
+	'config' => array(
+		'type' => 'user',
+		'userFunc' => 'Dkd\\SemanticImages\\UserFunction\\TcaFieldUserFunction->renderField'
+	)
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('sys_file_metadata', ',--div--;Image Context,image_context', '', '');
