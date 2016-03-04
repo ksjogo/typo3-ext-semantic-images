3023e07bf2b833e2d5df47285607db2a8bd1c175
dkd-goslar <johannes.goslar@dkd.de>
2016-03-04 17:40:01 +0100

    Flash Quality on Upload

--- Classes/Slots/FileUpload.php
+++ Classes/Slots/FileUpload.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dkd\SemanticImages\Slots;
+use Dkd\SemanticImages\Service\CalculateService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 use TYPO3\CMS\Core\Resource\Driver\AbstractDriver;
@@ -12,20 +13,26 @@ class FileUpload
 
     public function populateMetadata(\TYPO3\CMS\Core\Resource\FileInterface $file, \TYPO3\CMS\Core\Resource\Folder $folder)
     {
-        $this->flash("murks");
+
+        $qualities = GeneralUtility::makeInstance(CalculateService::class)->quality($file->getUid());
+        $message = '';
+        foreach($qualities as $key => $value)
+            $message .=  $key . ' => ' . $value . "\n";
+
+        $this->flash($message);
     }
 
     public function flash($message, $severity = \TYPO3\CMS\Core\Messaging\FlashMessage::OK)
-    {
-        $flashMessage = GeneralUtility::makeInstance(
-            \TYPO3\CMS\Core\Messaging\FlashMessage::class,
-            $message,
-            '',
-            $severity,
-            true
-        );
-        $flashMessageService = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessageService::class);
-        $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
-        $defaultFlashMessageQueue->enqueue($flashMessage);
-    }
+{
+    $flashMessage = GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Messaging\FlashMessage::class,
+        $message,
+        '',
+        $severity,
+        true
+    );
+    $flashMessageService = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessageService::class);
+    $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+    $defaultFlashMessageQueue->enqueue($flashMessage);
+}
 }
